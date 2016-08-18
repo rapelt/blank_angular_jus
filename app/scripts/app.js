9fa1bc5bb253b53cf1046219275f6b2ff8f59848
eru <epenkman@thoughtworks.com>
2016-08-18 13:39:19 +1000

    EP/KN set up angular project

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoApp
+ * @description
+ * # yoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
