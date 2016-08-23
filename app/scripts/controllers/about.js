6465e36191924d7153fd3f5f64b6ff8753e9aaa1
rapelt <rapelt@thoughtworks.com>
2016-08-23 14:42:17 +1000

    bex/eru css structure setup

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name yoApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the yoApp
- */
 angular.module('ossCafeApp')
   .controller('AboutCtrl', function () {
     this.awesomeThings = [
