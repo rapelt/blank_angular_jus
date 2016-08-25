90afa1a42c85b6a1068d60a4eca0ed09faadf39a
kuhle <knkonde@thoughtworks.com>
2016-08-25 17:00:12 +1000

    [Kuhle] Added Pages funtionality to make the test suite easily readable

---
+++ E2E/pages/landing/landingPage.js
@@ -0,0 +1,7 @@
+'use strict'; 
+
+var LandingPage = function (){
+
+
+};
+module.exports = new LandinPage();
---
+++ E2E/spec/landing/landingSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var landingPage = require('../pages/landing/landingPage.js');
+
+describe ('landing screen', function(){
+
+ it('should check our CAFE PAGE',function() {
+    browser.get('http://localhost:9001');
+    expect(true).toBe(true);
+  });
+
+	
+})
