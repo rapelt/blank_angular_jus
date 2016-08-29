28f3974e85d53e85304087b538f5963a8ba12265
Adetunji Sunmonu <tunji.sunmonu@gmail.com>
2016-08-29 17:35:06 +1000

    [TS] added test for un navigation

--- test/features/questions/questions-test.js
+++ test/features/questions/questions-test.js
@@ -2,19 +2,28 @@
 
 describe('Service questions controller', function(){
   beforeEach(module('ossCafeApp'));
-  var ablisDataService, scope, deferred;
+  var ablisDataService, scope, state, deferred;
 
   beforeEach(function(){
     ablisDataService = jasmine.createSpyObj('ablisDataService', ['getQuestions']);
-    inject(function($rootScope, $controller, $q, $templateCache){
+    inject(function($rootScope, $controller, $q, $templateCache, $state){
+      state = $state;
       scope = $rootScope.$new();
       $templateCache.put('features/home/main.html', '');
+      $templateCache.put('features/questions/questions.html', '');
 
       deferred = $q.defer();
       ablisDataService.getQuestions.and.returnValue(deferred.promise);
 
       $controller('QuestionsController', {$scope: scope, AblisDataService: ablisDataService});
     });
+  });
+
+  it('should load questions page', function () {
+      state.go('questions');
+      scope.$apply();
+      expect(state.current.controller).toBe('QuestionsController');
+      expect(state.current.url).toBe('/questions');
   });
 
   it('Should get questions', function(){
--- test/features/results/results-controller-test.js
+++ test/features/results/results-controller-test.js
@@ -2,19 +2,28 @@
 
 describe('Service results controller', function(){
   beforeEach(module('ossCafeApp'));
-  var ablisDataService, scope, deferred;
+  var ablisDataService, scope, state, deferred;
 
   beforeEach(function(){
     ablisDataService = jasmine.createSpyObj('ablisDataService', ['getResults']);
-    inject(function($rootScope, $controller, $q, $templateCache){
+    inject(function($rootScope, $controller, $q, $templateCache, $state){
+      state = $state;
       scope = $rootScope.$new();
       $templateCache.put('features/home/main.html', '');
+      $templateCache.put('features/results/_results.html', '');
 
       deferred = $q.defer();
       ablisDataService.getResults.and.returnValue(deferred.promise);
 
       $controller('ResultsController', {$scope: scope, AblisDataService: ablisDataService});
     });
+  });
+
+  it('should load results page', function () {
+      state.go('results');
+      scope.$apply();
+      expect(state.current.controller).toBe('ResultsController');
+      expect(state.current.url).toBe('/results');
   });
 
   it('Should get results', function(){
