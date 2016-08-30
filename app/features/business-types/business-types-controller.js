0f1dd8ba22fe394a06a9bd998224dc9eb8403f56
rapelt <rapelt@thoughtworks.com>
2016-08-30 13:53:58 +1000

    SC-12 [RA/TAS] created basic boxes

--- app/features/business-types/business-types-controller.js
+++ app/features/business-types/business-types-controller.js
@@ -2,6 +2,10 @@
 
 angular.module('Business-types', [])
   .controller('BusinessTypesController', function($scope, $state){
+    $scope.cafe = false;
+    $scope.mobileFood = false;
+    $scope.takeaway = false;
+
     $scope.showNextPage = function () {
       $state.go('questions');
     };
