0687bdb8267f4348e79c8f32f8a80fe9b8b21aeb
eru <epenkman@thoughtworks.com>
2016-08-31 11:29:34 +1000

    [EP] fix merge conflict

--- E2E/spec/landing/landingSpec.js
+++ E2E/spec/landing/landingSpec.js
@@ -17,14 +17,8 @@ describe('landing screen', function () {
   });
 
   it('should take you to the Questions', function () {
-<<<<<<< 584729eddc7337e9315c790e6c6148fd0728503d
     element.all(by.css('.business-type-box')).get(0).click();
     element.all(by.css('.business-type-box')).get(1).click();
     element(by.css('.oss-button')).click()
-=======
-    element.all(by.css('.business-type-box')).get(0).click()
-    element.all(by.css('.business-type-box')).get(1).click()
-    element(by.css('.os-button')).click()
->>>>>>> [TS] failed the functional test to verify build pipeline
   })
 });
