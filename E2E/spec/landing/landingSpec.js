2c5a6b6f560f5fc36ad047f71f8ce5217384b766
kuhle <knkonde@thoughtworks.com>
2016-08-31 12:13:48 +1000

    [EP/KN] refined homepage e2e test

--- E2E/spec/landing/landingSpec.js
+++ E2E/spec/landing/landingSpec.js
@@ -5,20 +5,17 @@ var landingPage = require('../../pages/landing/landingPage.js');
 
 describe('landing screen', function () {
   beforeEach(function () {
-    browser.get('http://localhost:9001')
-  });
-
-  it('should check we are on the main QLD page', function () {
-    expect(true).toBe(true)
-  });
+    browser.get('http://localhost:9001');
+  })
 
   it('should check the title', function () {
-    expect(browser.getCurrentUrl()).toContain('9001')
+    expect(browser.getCurrentUrl()).toContain("9001");
+    expect(landingPage.isCafeDisplayed()).toBeTruthy();
   });
 
   it('should take you to the Questions', function () {
     element.all(by.css('.business-type-box')).get(0).click();
     element.all(by.css('.business-type-box')).get(1).click();
     element(by.css('.oss-button')).click()
-  })
+  });
 });
