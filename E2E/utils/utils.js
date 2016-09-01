53a739a6b98da9e0bc7508017983e17dc4133917
Adetunji Sunmonu <tunji.sunmonu@gmail.com>
2016-09-01 21:14:49 +1000

    [TS]

--- E2E/utils/utils.js
+++ E2E/utils/utils.js
@@ -2,5 +2,7 @@ module.exports = function (callback) {
     browser.getCurrentUrl().then(function(url){
       console.log('Current url: ', url)
       callback();
+    }, function(error){
+      console.error('Error: ', error);
     });
 };
