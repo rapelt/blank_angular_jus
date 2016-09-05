bdb4862a70e53510586a1958ffaa32d97a4b5a11
Josh McCure <jmccure@thoughtworks.com>
2016-09-05 12:07:32 +1000

    [JM/KN] Added End to End to the Landing Spec

--- E2E/pages/landing/landingPage.js
+++ E2E/pages/landing/landingPage.js
@@ -7,5 +7,14 @@ var LandingPage = function (){
 	this.isCafeDisplayed = function(){
 		return this.triageChoice.get(0).isDisplayed();
 	};
+
+	this.clicksOnCafe = function(){
+		return this.triageChoice.get(0).click();
+	};
+
+	this.clicksOnMobileFood = function(){
+		return this.triageChoice.get(1).click();
+	};
+
 };
 module.exports = new LandingPage();
--- E2E/spec/landing/landingSpec.js
+++ E2E/spec/landing/landingSpec.js
@@ -3,6 +3,9 @@
 
 var waitForPage = require('../../utils/utils.js');
 var landingPage = require('../../pages/landing/landingPage.js');
+var questionsPage = require('../../pages/questions/questionsPage.js');
+var resultsPage = require('../../pages/results/resultsPage.js');
+
 
 describe('landing screen', function () {
   beforeEach(function () {
@@ -16,11 +19,20 @@ describe('landing screen', function () {
     });
   });
 
-  it('should take you to the Questions', function () {
+  it('Should Test the End to End User Flow', function () {
     waitForPage(function(){
-      element.all(by.css('.box')).get(0).click();
-      element.all(by.css('.box')).get(1).click();
-      element(by.css('.oss-button')).click()
+      landingPage.clicksOnCafe();
+      landingPage.clicksOnMobileFood();
+      questionsPage.next();
+      // On Questions Page
+      expect(questionsPage.questionCheckbox.get(1).waitReady()).toBeTruthy();
+      questionsPage.question();
+      expect(questionsPage.questionSubcheckbox.get(0).waitReady()).toBeTruthy();
+      questionsPage.question2();
+      questionsPage.next();
+      questionsPage.next();
+      //On the Results Page
+      resultsPage.expandCategoryOne();
     });
   });
 });
