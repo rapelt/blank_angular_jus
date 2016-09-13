a9717c27f377809611e2f5be8c5415a77b12fc71
rapelt <rapelt@thoughtworks.com>
2016-09-13 16:18:39 +1000

    RA - removed e2e tests for cafe

--- E2E/pages/landing/landingPage.js
+++ E2E/pages/landing/landingPage.js
@@ -1,55 +1,10 @@
 'use strict';
 
 var LandingPage = function () {
-  this.allBusinessActivities = element.all(by.css('.box'));
-  this.cafeBusinessActivity = element(by.model('businessActivities.cafe'));
-  this.mobileFoodBusinessActivity = element(by.model('businessActivities.mobile'));
-  this.takeawayBusinessActivity = element(by.model('businessActivities.takeaway'));
-  this.nextButton = element(by.css('.jus-next-button'));
-  this.startButton = element(by.id('jus-start-button'));
-
-  this.checkNextButtonEnabled = function () {
-    return this.nextButton.isEnabled();
-  };
-
-  this.checkBusinessActivitiesVisible = function () {
-    return this.allBusinessActivities.count();
-  };
-
-  this.selectAllBusinessActivities = function () {
-    return this.allBusinessActivities.click();
-  };
-
-  this.startButtonNext = function () {
-    return this.startButton.click();
-  };
-
-  this.areAllBusinessActivitiesSelected = function () {
-    return this.allBusinessActivities.isSelected();
-  };
-
-  this.isCafeBusinessActivitySelected = function () {
-    return this.cafeBusinessActivity.isSelected();
-  };
-
-  this.isMobileFoodBusinessActivitySelected = function () {
-    return this.mobileFoodBusinessActivity.isSelected();
-  };
-
-  this.isTakeawayBusinessActivitySelected = function () {
-    return this.takeawayBusinessActivity.isSelected();
-  };
-
-  this.isCafeDisplayed = function () {
-    return this.allBusinessActivities.get(0).isDisplayed();
-  };
-
-  this.selectCafe = function () {
-    return this.allBusinessActivities.get(0).click();
-  };
+  this.homePage = element(by.css('.home-page'));
 
   this.selectMobileFood = function () {
-    return this.allBusinessActivities.get(1).click();
+    return this.homePage.get(1);
   };
 };
 module.exports = new LandingPage();
--- E2E/spec/landing/landingSpec.js
+++ E2E/spec/landing/landingSpec.js
@@ -6,26 +6,11 @@ var landingPage = require('../../pages/landing/landingPage.js');
 describe('landing screen', function () {
   beforeEach(function () {
     browser.driver.get(waitForPage.URL + '/#');
-    landingPage.startButtonNext();
   });
 
-  it('Verify next button is disabled with no selected Business Activity', function () {
+  it('Verify the home page is hit', function () {
     waitForPage.wait(function () {
-      expect(landingPage.checkNextButtonEnabled()).toBeFalsy();
-    });
-  });
-
-  it('Verify all 3 Business Activities are Visible', function () {
-    waitForPage.wait(function () {
-      expect(landingPage.checkBusinessActivitiesVisible()).toEqual(3);
-    });
-  });
-
-  xit('Verify all 3 Business Activities can be selected', function () {
-    waitForPage.wait(function () {
-      landingPage.selectAllBusinessActivities();
-      expect(landingPage.areAllBusinessActivitiesSelected()).toBeTruthy();
-      expect(landingPage.checkNextButtonEnabled()).toBeTruthy();
+      landingPage.selectMobileFood();
     });
   });
 });
